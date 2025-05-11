5b3aed402149c6ba94199bdab981aedac3f2c537
root4nishant <srivastava4nishant@gmail.com>
2025-05-11 19:07:45 +0530

    Auto commit

--- config/config.sh
+++ config/config.sh
@@ -1,4 +1,4 @@
 API_KEY="AIzaSyAYyNrR0Gvj6TOvHmEni31fFBKCeaGa0Qw"
 MODEL_NAME="gemini-2.0-flash"
 
-PYTHON_PATH="/c/Users/username/AppData/Local/Programs/Python/Python313/python.exe"
+PYTHON_PATH="/c/Users/root4nishant/AppData/Local/Programs/Python/Python313/python.exe"
